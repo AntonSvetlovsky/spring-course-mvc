e81b9ead80fcada8e6f62f635738ef069e7e9383
Anton <antonsvetlovsky@gmail.com>
2022-08-10 02:58:41 +0300

    Impl task

--- src/main/java/org/example/spring/rest/App.java
+++ src/main/java/org/example/spring/rest/App.java
@@ -1,4 +1,31 @@
 package org.example.spring.rest;
 
+import org.example.spring.rest.configuration.MyConfig;
+import org.example.spring.rest.entity.Employee;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
 public class App {
+
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+        Communication communication = context.getBean("communication", Communication.class);
+
+        List<Employee> allEmployees = communication.getAllEmployees();
+        System.out.println(allEmployees);
+
+//        Employee empById = communication.getEmployee(1);
+//        System.out.println(empById);
+//
+//        Employee emp = new Employee("Amy", "McDonald", "HR", 1950);
+//        communication.saveEmployee(emp);
+
+//        Employee emp = new Employee("Amy", "McDonald", "HR", 2100);
+//        emp.setId(9);
+//        communication.saveEmployee(emp);
+
+//        communication.deleteEmployee(5);
+    }
 }
