e72763ae91060561ae888913e0729c9dc3fe8c40
Anton <antonsvetlovsky@gmail.com>
2022-08-12 15:51:02 +0300

    v2

--- src/main/java/org/example/spring/springboot/springboot_rest/controller/MyRestController.java
+++ src/main/java/org/example/spring/springboot/springboot_rest/controller/MyRestController.java
@@ -3,9 +3,7 @@ package org.example.spring.springboot.springboot_rest.controller;
 import org.example.spring.springboot.springboot_rest.entity.Employee;
 import org.example.spring.springboot.springboot_rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,37 +20,37 @@ public class MyRestController {
         return employeeService.getAllEmployees();
     }
 
-//    @GetMapping("employees/{id}")
-//    public Employee getEmployee(@PathVariable int id) {
-//
-//        Employee employee = employeeService.getEmployee(id);
-//
-//        return employee;
-//    }
-//
-//    @PostMapping("/employees")
-//    public Employee addNewEmployee(@RequestBody Employee employee) {
-//
-//        employeeService.saveEmployee(employee);
-//
-//        return employee;
-//    }
-//
-//    @PutMapping("/employees")
-//    public Employee updateEmployee(@RequestBody Employee employee) {
-//
-//        employeeService.saveEmployee(employee);
-//
-//        return employee;
-//    }
-//
-//    @DeleteMapping("employees/{id}")
-//    public String deleteEmployee(@PathVariable int id) {
-//
-//        Employee employee = employeeService.getEmployee(id);
-//
-//        employeeService.deleteEmployee(id);
-//
-//        return "Employee with ID = " + id + " was deleted.";
-//    }
+    @GetMapping("employees/{id}")
+    public Employee getEmployee(@PathVariable int id) {
+
+        Employee employee = employeeService.getEmployee(id);
+
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee) {
+
+        employeeService.saveEmployee(employee);
+
+        return employee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+
+        employeeService.saveEmployee(employee);
+
+        return employee;
+    }
+
+    @DeleteMapping("employees/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+
+        Employee employee = employeeService.getEmployee(id);
+
+        employeeService.deleteEmployee(id);
+
+        return "Employee with ID = " + id + " was deleted.";
+    }
 }
