1a682e628a831395aeeadd338dba7c4035d05e11
Anton <antonsvetlovsky@gmail.com>
2022-08-14 04:05:22 +0300

    v1

--- src/main/java/org/example/spring/springboot/spring_data_jpa/controller/MyRestController.java
+++ src/main/java/org/example/spring/springboot/spring_data_jpa/controller/MyRestController.java
@@ -53,4 +53,11 @@ public class MyRestController {
 
         return "Employee with ID = " + id + " was deleted.";
     }
+
+    @GetMapping("employees/name/{name}")
+    public List<Employee> showAllEmployeesByName(@PathVariable String name) {
+        List<Employee> employees = employeeService.findAllByName(name);
+
+        return employees;
+    }
 }
--- src/main/java/org/example/spring/springboot/spring_data_jpa/dao/EmployeeRepository.java
+++ src/main/java/org/example/spring/springboot/spring_data_jpa/dao/EmployeeRepository.java
@@ -3,5 +3,9 @@ package org.example.spring.springboot.spring_data_jpa.dao;
 import org.example.spring.springboot.spring_data_jpa.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    List<Employee> findAllByName(String name);
 }
